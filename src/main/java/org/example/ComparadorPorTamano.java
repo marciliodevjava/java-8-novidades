2782c12783571a6014de2ae2a37c3a085183132e
Marcilio Zanatta <marcsilva@brasilseg.com.br>
2023-04-13 22:19:35 -0300

    Atualizando

---
+++ src/main/java/org/example/ComparadorPorTamano.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Comparator;
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+
+        if (s1.length() < s2.length()) return -1;
+        if (s1.length() > s2.length()) return 1;
+        return 0;
+    }
+
+}
--- src/main/java/org/example/OrdenaStrings.java
+++ src/main/java/org/example/OrdenaStrings.java
@@ -75,15 +75,3 @@ public class OrdenaStrings {
     }
 }
 
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-
-        if (s1.length() < s2.length()) return -1;
-        if (s1.length() > s2.length()) return 1;
-        return 0;
-    }
-
-}
